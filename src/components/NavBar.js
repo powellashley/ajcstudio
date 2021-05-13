8fdee83ead883138410b965285fcc9ee8be557f1
Ashley Powell <ashleypowell@Ashleys-MacBook-Pro-2.local>
2021-05-13 12:22:38 +0100

    useEffect edit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import { useEffect } from "react/cjs/react.development";
 import { Button } from './Button';
 import './NavBar.css';
 
